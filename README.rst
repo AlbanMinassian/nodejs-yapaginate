ff67fa4eba6ae1f45e1aefc1859f51a4679f5554
alban <alban@alban-EL1352.(none)>
2012-06-05 18:33:42 +0200

    add option showStep and debug

--- README.rst
+++ README.rst
@@ -43,10 +43,11 @@ params        {}                  your personnal params add to link : if {aa:bb}
 parampageno   'pageno'            key name add to link : url?pageno=10
 showPrevNext  true                show previous and next button 
 showFirstLast true                show first and last button   
-hideDots      false               hide dots separator
-DotsMidSize   4                   show X step before and after current page ( if hideDots = true )
-DotsEndSize   2                   show X step after first and before end page ( if hideDots = true )
-dotText       '&#8230;'           text for dots separator ( if hideDots = true )
+showStep      true                show step page. Show or Hide [1][2][3][4]...
+hideDots      false               hide dots separator ( if showStep = true ) 
+DotsMidSize   4                   show X step before and after current page ( if hideDots = false )
+DotsEndSize   2                   show X step after first and before end page ( if hideDots = false )
+dotText       '&#8230;'           text for dots separator ( if hideDots = false )
 firstText     '&laquo&laquo'      text for goto first page
 prevText      '&laquo'            text for goto previous page
 nextText      '&raquo'            text for goto next page
@@ -57,4 +58,4 @@ showCR        false               add \\n (only for debug)
 
 Licence
 ------------------------------------------
-Code licensed under the `GNU GENERAL PUBLIC LICENSE Version 3 <http://www.gnu.org/copyleft/gpl.html>`_
+Code licensed under the `GNU GENERAL PUBLIC LICENSE <http://www.gnu.org/copyleft/gpl.html>`_ Version 3 
--- test/main.js
+++ test/main.js
@@ -5,9 +5,33 @@ var paginate = require('../lib/main');
 
 describe('paginate', function() {
     describe('with no arguments', function() {
-        it('returns a valid html without option', function() {
+        it('return valid html without option', function() {
+            
             var result = paginate();
-            result.should.eql('<div class="pagination"><ul><li class="disable"><a href="#">&laquo&laquo</a></li><li class="disable"><a href="#">&laquo</a></li><li class="active"><a href="#">1</a></li><li><a href="/unknowurl?pageno=2">2</a></li><li><a href="/unknowurl?pageno=3">3</a></li><li><a href="/unknowurl?pageno=4">4</a></li><li><a href="/unknowurl?pageno=5">5</a></li><li class="disabled"><a href="#">&#8230;</a></li><li><a href="/unknowurl?pageno=99">99</a></li><li><a href="/unknowurl?pageno=100">100</a></li><li><a href="/unknowurl?pageno=2">&raquo</a></li><li><a href="/unknowurl?pageno=100">&raquo&raquo</a></li></ul></div>');
+            result.should.eql('<div class="pagination"><ul>\
+<li class="disable"><a href="#">&laquo&laquo</a></li>\
+<li class="disable"><a href="#">&laquo</a></li>\
+<li class="active"><a href="#">1</a></li>\
+<li><a href="/unknowurl?pageno=2">2</a></li>\
+<li><a href="/unknowurl?pageno=3">3</a></li>\
+<li><a href="/unknowurl?pageno=4">4</a></li>\
+<li><a href="/unknowurl?pageno=5">5</a></li>\
+<li class="disabled"><a href="#">&#8230;</a></li>\
+<li><a href="/unknowurl?pageno=99">99</a></li>\
+<li><a href="/unknowurl?pageno=100">100</a></li>\
+<li><a href="/unknowurl?pageno=2">&raquo</a></li>\
+<li><a href="/unknowurl?pageno=100">&raquo&raquo</a></li>\
+</ul></div>');
+        });
+        
+        it('return html witout step', function() {
+            var result = paginate({showStep:false});
+            result.should.eql('<div class="pagination"><ul>\
+<li class="disable"><a href="#">&laquo&laquo</a></li>\
+<li class="disable"><a href="#">&laquo</a></li>\
+<li><a href="/unknowurl?pageno=2">&raquo</a></li>\
+<li><a href="/unknowurl?pageno=100">&raquo&raquo</a></li>\
+</ul></div>');
         });
     });
 });
