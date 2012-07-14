5cfcb29886df927e81875c6d23ec576ff46b9c09
ami44 <alban.minassian@free.fr>
2012-07-14 21:35:30 +0200

    add setJson option

--- test/main.js
+++ test/main.js
@@ -126,6 +126,43 @@ describe('paginate', function() {
             underscore.isEqual(result, shouldResult).should.eql( true );
         });
         
+        // ----------------------------------------------------------
+        // test setJson
+        // ----------------------------------------------------------
+        it('return html from json', function() {
+            
+            var result = paginate({setJson: {
+                options: { showFirstLast: true, showPrevNext: true, showStep: true },
+                first: { cssclass: 'disable', href: '#', text: '&laquo;&laquo;' },
+                previous: { cssclass: 'disable', href: '#', text: '&laquo;' },
+                step: [ 
+                    { ispageno: true, cssclass: 'active', href: '#', text: '1' },
+                    { ispageno: true, cssclass: '', href: '/unknowurl?pageno=2', text: '2' },
+                    { ispageno: true, cssclass: '', href: '/unknowurl?pageno=3', text: '3' },
+                    { ispageno: true, cssclass: '', href: '/unknowurl?pageno=4', text: '4' },
+                    { ispageno: true, cssclass: '', href: '/unknowurl?pageno=5', text: '5' },
+                    { ispageno: false, cssclass: 'disabled', href: '#', text: '&#8230;' },
+                    { ispageno: true, cssclass: '', href: '/unknowurl?pageno=99', text: '99' },
+                    { ispageno: true, cssclass: '', href: '/unknowurl?pageno=100', text: '100' } 
+                ],
+                next: { cssclass: '', href: '/unknowurl?pageno=2', text: '&raquo;' },
+                last: { cssclass: '', href: '/unknowurl?pageno=100', text: '&raquo;&raquo;' } 
+            }});
+            result.should.eql('<div class="pagination"><ul>\
+<li class="disable"><a href="#">&laquo;&laquo;</a></li>\
+<li class="disable"><a href="#">&laquo;</a></li>\
+<li class="active"><a href="#">1</a></li>\
+<li class=""><a href="/unknowurl?pageno=2">2</a></li>\
+<li class=""><a href="/unknowurl?pageno=3">3</a></li>\
+<li class=""><a href="/unknowurl?pageno=4">4</a></li>\
+<li class=""><a href="/unknowurl?pageno=5">5</a></li>\
+<li class="disabled"><a href="#">&#8230;</a></li>\
+<li class=""><a href="/unknowurl?pageno=99">99</a></li>\
+<li class=""><a href="/unknowurl?pageno=100">100</a></li>\
+<li class=""><a href="/unknowurl?pageno=2">&raquo;</a></li>\
+<li class=""><a href="/unknowurl?pageno=100">&raquo;&raquo;</a></li>\
+</ul></div>');
+        });
         
         
         
